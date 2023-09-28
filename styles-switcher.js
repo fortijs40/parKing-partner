3449dd19651065b8e2b6354d589d26d699e76dc4
Marks-a <73535565+Marks-a@users.noreply.github.com>
2023-09-28 21:22:34 +0300

    Add files via upload

--- styles-switcher.js
+++ styles-switcher.js
@@ -1,10 +1,10 @@
- // function to set a given theme/color-scheme
+ 
  function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
 }
 
-// function to toggle between light and dark theme
+
 function toggleTheme() {
     if (localStorage.getItem('theme') === 'theme-dark') {
         setTheme('theme-light');
@@ -13,7 +13,7 @@ function toggleTheme() {
     }
 }
 
-// Immediately invoked function to set the theme on initial load
+
 (function () {
     if (localStorage.getItem('theme') === 'theme-dark') {
         setTheme('theme-dark');
