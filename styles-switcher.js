3cbc78d9f181b209aad5f9731d07d6176f4560c7
Marks-a <73535565+Marks-a@users.noreply.github.com>
2023-09-29 16:01:19 +0300

    Add files via upload

--- styles-switcher.js
+++ styles-switcher.js
@@ -1,23 +1,36 @@
- 
- function setTheme(themeName) {
-    localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
-}
+// function setTheme(themeName) {
+//     localStorage.setItem('theme', themeName);
+//     document.documentElement.className = themeName;
+// }
 
 
+// function toggleTheme() {
+//     if (localStorage.getItem('theme') === 'theme-dark') {
+//         setTheme('theme-light');
+//     } else {
+//         setTheme('theme-dark');
+//     }
+// }
+
+
+// (function () {
+//     if (localStorage.getItem('theme') === 'theme-dark') {
+//         setTheme('theme-dark');
+//     } else {
+//         setTheme('theme-light');
+//     }
+// })();
+const themeSwitcher = document.getElementById("style-toggle.js");
+themeSwitcher.addEventListener("change", toggleTheme);
 function toggleTheme() {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-light');
-    } else {
-        setTheme('theme-dark');
+    const body = document.body;
+    if (themeSwitcher.checked) {
+        body.classList.remove("theme-light");
+        body.classList.add("theme-dark");
+    } else {      
+        body.classList.remove("theme-dark");
+        body.classList.add("theme-light");
     }
 }
 
 
-(function () {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-dark');
-    } else {
-        setTheme('theme-light');
-    }
-})();
