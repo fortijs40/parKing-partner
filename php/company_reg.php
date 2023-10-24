332e20089ba877c34f01635a56ccd77036a2d41f
fortijs40 <xralfsx@inbox.lv>
2023-10-24 20:12:50 +0300

    small stuff

--- php/company_reg.php
+++ php/company_reg.php
@@ -149,5 +149,5 @@ try {
 
 echo '<br>';
 
-header('./login.html');
+header('./login.php');
 exit;
--- php/person_reg.php
+++ php/person_reg.php
@@ -151,5 +151,5 @@ try {
 echo '<br>';
 
 
-header('./login.html');
+header('./login.php');
 exit;
