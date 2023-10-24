19f3f20f3108f3893fb6035cb6bebd41efd0abcf
fortijs40 <xralfsx@inbox.lv>
2023-10-24 19:27:41 +0300

    my sql

---
+++ partners (1).sql
@@ -0,0 +1,228 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 24, 2023 at 04:08 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `partners`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `companies`
+--
+
+DROP TABLE IF EXISTS `companies`;
+CREATE TABLE IF NOT EXISTS `companies` (
+  `company_id` bigint NOT NULL AUTO_INCREMENT,
+  `partner_id` bigint NOT NULL,
+  `company_name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `reg_no` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `email` varchar(60) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `phone_number` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `second_phone_no` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `bank_account` varchar(60) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `billing_address` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  PRIMARY KEY (`company_id`),
+  KEY `partner_id` (`partner_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parkingspots`
+--
+
+DROP TABLE IF EXISTS `parkingspots`;
+CREATE TABLE IF NOT EXISTS `parkingspots` (
+  `spot_id` bigint NOT NULL AUTO_INCREMENT,
+  `partner_id` bigint NOT NULL,
+  `spot_name` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `spot_address` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `max_spot_count` int NOT NULL,
+  `is_premium` tinyint DEFAULT NULL,
+  `is_disabled` tinyint DEFAULT NULL,
+  `add_info` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `rating` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`spot_id`),
+  KEY `partner_id` (`partner_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `partners_id`
+--
+
+DROP TABLE IF EXISTS `partners_id`;
+CREATE TABLE IF NOT EXISTS `partners_id` (
+  `partner_id` bigint NOT NULL AUTO_INCREMENT,
+  `type_id` tinyint NOT NULL,
+  `email` varchar(60) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `hashed_password` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  PRIMARY KEY (`partner_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `partner_types`
+--
+
+DROP TABLE IF EXISTS `partner_types`;
+CREATE TABLE IF NOT EXISTS `partner_types` (
+  `type_id` tinyint NOT NULL AUTO_INCREMENT,
+  `type_description` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  PRIMARY KEY (`type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `persons`
+--
+
+DROP TABLE IF EXISTS `persons`;
+CREATE TABLE IF NOT EXISTS `persons` (
+  `person_id` bigint NOT NULL AUTO_INCREMENT,
+  `partner_id` bigint NOT NULL,
+  `first_name` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `last_name` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `email` varchar(60) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `phone_number` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `bank_account` varchar(60) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `billing_address` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  PRIMARY KEY (`person_id`),
+  KEY `partner_id` (`partner_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+DROP TABLE IF EXISTS `reports`;
+CREATE TABLE IF NOT EXISTS `reports` (
+  `rep_id` bigint NOT NULL AUTO_INCREMENT,
+  `partner_id` bigint NOT NULL,
+  `client_id` bigint NOT NULL,
+  `spot_id` bigint DEFAULT NULL,
+  `rep_description` text COLLATE utf8mb3_unicode_ci,
+  `is_read` tinyint DEFAULT NULL,
+  PRIMARY KEY (`rep_id`),
+  KEY `partner_id` (`partner_id`),
+  KEY `spot_id` (`spot_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservations`
+--
+
+DROP TABLE IF EXISTS `reservations`;
+CREATE TABLE IF NOT EXISTS `reservations` (
+  `reserv_id` bigint NOT NULL AUTO_INCREMENT,
+  `partner_id` bigint NOT NULL,
+  `client_id` bigint NOT NULL,
+  `spot_id` bigint DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `start_time` time DEFAULT NULL,
+  `end_time` time DEFAULT NULL,
+  `parkingspot` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `payment_sum` int DEFAULT NULL,
+  `is_read` tinyint DEFAULT NULL,
+  PRIMARY KEY (`reserv_id`),
+  KEY `partner_id` (`partner_id`),
+  KEY `spot_id` (`spot_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+DROP TABLE IF EXISTS `reviews`;
+CREATE TABLE IF NOT EXISTS `reviews` (
+  `rev_id` bigint NOT NULL AUTO_INCREMENT,
+  `partner_id` bigint NOT NULL,
+  `client_id` bigint NOT NULL,
+  `spot_id` bigint DEFAULT NULL,
+  `rev_description` text COLLATE utf8mb3_unicode_ci,
+  `posted_time` datetime(6) DEFAULT NULL,
+  `rating` double NOT NULL,
+  `title` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `is_read` tinyint DEFAULT NULL,
+  PRIMARY KEY (`rev_id`),
+  KEY `partner_id` (`partner_id`),
+  KEY `spot_id` (`spot_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `companies`
+--
+ALTER TABLE `companies`
+  ADD CONSTRAINT `companies_ibfk_1` FOREIGN KEY (`partner_id`) REFERENCES `partners_id` (`partner_id`);
+
+--
+-- Constraints for table `parkingspots`
+--
+ALTER TABLE `parkingspots`
+  ADD CONSTRAINT `parkingspots_ibfk_1` FOREIGN KEY (`partner_id`) REFERENCES `partners_id` (`partner_id`);
+
+--
+-- Constraints for table `persons`
+--
+ALTER TABLE `persons`
+  ADD CONSTRAINT `persons_ibfk_1` FOREIGN KEY (`partner_id`) REFERENCES `partners_id` (`partner_id`);
+
+--
+-- Constraints for table `reports`
+--
+ALTER TABLE `reports`
+  ADD CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`partner_id`) REFERENCES `partners_id` (`partner_id`),
+  ADD CONSTRAINT `reports_ibfk_2` FOREIGN KEY (`spot_id`) REFERENCES `parkingspots` (`spot_id`);
+
+--
+-- Constraints for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`partner_id`) REFERENCES `partners_id` (`partner_id`),
+  ADD CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`spot_id`) REFERENCES `parkingspots` (`spot_id`);
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`partner_id`) REFERENCES `partners_id` (`partner_id`),
+  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`spot_id`) REFERENCES `parkingspots` (`spot_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
